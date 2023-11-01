7f92b88f2de09306d752b8100476226d5cad52b7
Kpranav <117839743+kpranav04@users.noreply.github.com>
2023-11-01 17:05:51 +0530

    book

--- server/server.js
+++ server/server.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
-
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
-
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
-
 const app = express();
+const AuthRoute=require('./routes/auth')
 
+
+//DATABASE CONNECTION
+
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "",
+  database: "counselling",
+});
+
+
+//MIDDLEWARES
 app.use(express.json());
 app.use(
   cors({
@@ -21,14 +29,23 @@ app.use(
 );
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-const db = mysql.createConnection({
-  user: "root",
-  host: "localhost",
-  password: "",
-  database: "employeesystem",
+//ERROR MIDDLEWARE
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
 });
+//ROUTE MIDDLEWARE
+app.use('/auth', AuthRoute);
 
+
+
+//LISTENING PORT
 app.listen(3001, () => {
     console.log("Yey, your server is running on port 3001");
   });
